9be38bf75efc59a3d5d5da7d76ec711fd297f07b
godwinn22 <godwinohazulike@gmail.com>
2024-03-01 23:26:40 +0100

    Solved task 8. Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -12,9 +12,3 @@ lists = [Format[0].compact, Format[1].compact, Format[2].compact]
 result = lists.join(',')
 
 puts result
-
-
-# #!/usr/bin/env ruby
-# Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-# List = [Format[0].compact, Format[1].compact, Format[2].compact]
-# puts List.join(',')
