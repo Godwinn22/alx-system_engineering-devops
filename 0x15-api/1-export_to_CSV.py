ec6905dc9c1381844539eb51547b0c7753a1bcb5
godwinn22 <godwinohazulike@gmail.com>
2024-05-22 16:26:16 +0100

    Solved task 1, Extended my Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,12 +18,12 @@ if __name__ == "__main__":
     emp_name = user.json().get("name")
     # getting the todos
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    
+
     filename = user_id + '.csv'
     with open(filename, mode='w') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL, lineterminator='\n')
-        
+
         for todo in todos.json():
             if todo.get('userId') == int(user_id):
                 writer.writerow([user_id, emp_name, str(todo.get(
