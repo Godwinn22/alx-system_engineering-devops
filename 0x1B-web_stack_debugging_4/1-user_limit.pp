e1906316f0174ec68e022a0f4ab84b251a6bf5a3
godwinn22 <godwinohazulike@gmail.com>
2024-06-13 13:46:13 +0100

    Done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,13 +5,13 @@
 # Increase hard file limit for holberton user
 
 exec { 'holberton-hard-nofile':
-  command => "echo 'holberton hard nofile 50000' >> /etc/security/limits.conf",
+  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
   path   => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft file limit for holberton user
 
 exec { 'holberton-soft-nofile':
-  command => "echo 'holberton soft nofile 50000' >> /etc/security/limits.conf",
+  command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
   path   => '/usr/local/bin/:/bin/'
 }
